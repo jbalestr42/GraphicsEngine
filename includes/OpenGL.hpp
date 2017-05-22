21fde927653ffa18c1c7a05f8307df7c210cee34
jbalestr42 <julien.balestrieri@gmail.com>
2017-05-22 10:54:29 +0200

    clean cmake on mac and fix issue with flags

--- includes/OpenGL.hpp
+++ includes/OpenGL.hpp
@@ -1,8 +1,9 @@
 #ifndef OPENGL_HPP
 # define OPENGL_HPP
 
-// Mac don't need glew
 # if defined(MACOS)
+#  define __gl_h_
+#  define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
 #  include <OpenGL/gl3.h>
 # else
 #  include <GL/glew.h>
